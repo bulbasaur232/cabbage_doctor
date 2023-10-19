85adc31dd4e8fb014c66110a5837ac969ad86fec
bulbasaur232 <rdd2033@naver.com>
2023-10-19 17:39:16 +0900

    진단 화면 디자인 변경

--- lib/classify/s_classify.dart
+++ lib/classify/s_classify.dart
@@ -16,26 +16,36 @@ class Classify extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: navigationBar(title: '진단하기'),
+      backgroundColor: const Color(0xffedeeed),
       child: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(height: 10),
-            const _Button(
-                color: CupertinoColors.systemGreen,
-                icon: CupertinoIcons.camera,
-                text: '촬영해서 진단하기',
-                imageSource: ImageSource.camera),
-            const _Button(
-                color: CupertinoColors.systemGreen,
-                icon: CupertinoIcons.photo_on_rectangle,
-                text: '사진파일로 진단하기',
-                imageSource: ImageSource.gallery),
-            Image.asset(
-              '$imgPath/haeboseyo.png',
-              width: 250,
-              height: 250,
+        child: Center(
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+            height: 600,
+            decoration: BoxDecoration(color: CupertinoColors.systemBackground, borderRadius: BorderRadius.circular(10 )),
+            child: Column(
+              children: [
+                const _Button(
+                    color: Color(0xff85cd97),
+                    icon: CupertinoIcons.camera,
+                    text: '촬영으로 진단하기',
+                    imageSource: ImageSource.camera),
+                const SizedBox(height: 20),
+                const _Button(
+                    color: Color(0xff7ec2d3),
+                    icon: CupertinoIcons.photo_on_rectangle,
+                    text: '사진파일로 진단하기',
+                    imageSource: ImageSource.gallery),
+                const SizedBox(height: 20),
+                Image.asset(
+                  '$imgPath/wonchu.png',
+                  width: 230,
+                  height: 160,
+                )
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
@@ -57,44 +67,42 @@ class _Button extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: () async {
-          /// 이미지 가져오기
-          final image = await picker.pickImage(source: imageSource);
-          if (image == null) return;
+    return GestureDetector(
+      onTap: () async {
+        /// 이미지 가져오기
+        final image = await picker.pickImage(source: imageSource);
+        if (image == null) return;
 
-          /// 이미지 판독후 상위 3개 결과 추출
-          final predictResult =
-              classifier.predict(img.decodeImage(await image.readAsBytes())!);
-          final resultNum = predictResult.length < 3 ? predictResult.length : 3;
-          final resultList = predictResult.getRange(0, resultNum).toList();
+        /// 이미지 판독후 상위 3개 결과 추출
+        final predictResult =
+            classifier.predict(img.decodeImage(await image.readAsBytes())!);
+        final resultNum = predictResult.length < 3 ? predictResult.length : 3;
+        final resultList = predictResult.getRange(0, resultNum).toList();
 
-          /// 판독결과 가지고 결과 페이지 라우팅
-          if (!context.mounted) return;
-          Navigator.of(context).push(CupertinoPageRoute(
-              builder: (context) => ResultScreen(resultList, image)));
-        },
-        child: Center(
-          child: Container(
-            height: 200,
-            width: 350,
-            decoration: BoxDecoration(
-                color: color, borderRadius: BorderRadius.circular(8)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(icon, color: CupertinoColors.white, size: 65),
-                const SizedBox(height: 15),
-                Text(
-                  text,
-                  style: const TextStyle(
-                      color: CupertinoColors.white,
-                      fontSize: 26,
-                      fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
+        /// 판독결과 가지고 결과 페이지 라우팅
+        if (!context.mounted) return;
+        Navigator.of(context).push(CupertinoPageRoute(
+            builder: (context) => ResultScreen(resultList, image)));
+      },
+      child: Center(
+        child: Container(
+          height: 180,
+          width: 350,
+          decoration: BoxDecoration(
+              color: color, borderRadius: BorderRadius.circular(5)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, color: CupertinoColors.white, size: 65),
+              const SizedBox(height: 15),
+              Text(
+                text,
+                style: const TextStyle(
+                    color: CupertinoColors.white,
+                    fontSize: 26,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
           ),
         ),
       ),
